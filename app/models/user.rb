229e6fd8883e704aef8082f7fb89ae37bc3d1756
Celso Ribeiro <celsofilho82@gmail.com>
2020-03-11 17:44:16 -0300

    Seed user completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :sent_messages, :class_name => "Message", :foreign_key => "sender_id"
   has_many :received_messages, :class_name => "Message", :foreign_key => "receiver_id"
   validates :username, presence: true
+  has_one_attached :avatar
   has_many_attached :photos
   has_many :interactions
   belongs_to :state, optional: true
