b7c1a455953a9eb96a3d26673dc090a471355d07
Andres Hernandez <aahrodriguez@gmail.com>
2020-03-12 12:49:44 -0300

    cuasi navbar de messages done e show/user mostra image do match

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,5 +3,8 @@ class Match < ApplicationRecord
   belongs_to :user_one, class_name: "User"
   belongs_to :user_two, class_name: "User"
 
-
+  def other_user(user)
+    return user_two if user == user_one
+    user_one
+  end
 end
