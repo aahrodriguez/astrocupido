46015f8b926685b22246277b2b82adb1111add49
Marcele Godoy <marcelegodoy@gmail.com>
2020-03-12 12:48:53 -0300

    Lots of interface changes

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,6 +2,4 @@ class Match < ApplicationRecord
   has_many :messages
   belongs_to :user_one, class_name: "User", dependent: :destroy
   belongs_to :user_two, class_name: "User", dependent: :destroy
-
-
 end
